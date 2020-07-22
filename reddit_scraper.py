053103f4f33435d887d3849fb181ab00e986575c
gkjohns <gkjohns@gmail.com>
2020-07-21 19:27:29 -0700

    Refactor logic to get a random quote

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -1,21 +1,27 @@
 import praw
 
+REDDIT_SESSION_DATA = {
+    'client_id': 'wizRreVm_MG4ZQ',
+    'client_secret': 'YWVII1qb3s2ULNUykaBWPec5HLs',
+    # password="1guiwevlfo00esyy",
+    'user_agent': 'yerrrrr',
+    'username': 'kyledrogo'
+}
+
+def get_random_post(session_data):
+
+    reddit = praw.Reddit(**session_data)
+    post_generator = reddit.subreddit('quotes').random_rising()
+    post = next(post_generator)
+
+    return post.title
+
 
 def main():
 
-    reddit = praw.Reddit(
-        client_id="wizRreVm_MG4ZQ",
-        client_secret="YWVII1qb3s2ULNUykaBWPec5HLs",
-        # password="1guiwevlfo00esyy",
-        user_agent="yerrrrr",
-        username="kyledrogo"
-    )
-
-    post_generator = reddit.subreddit('quotes').random_rising()
-    post = next(post_generator)
-    quote = post.title
-
+    quote = get_random_post(REDDIT_SESSION_DATA)
     print(quote)
+
 
 if __name__ == '__main__':
     main()
